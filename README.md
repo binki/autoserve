0bcb31288a5cadb64d640a03804e373e896bba6a
Nathan Phillip Brink <binki@gentoo.org>
2016-08-23 05:48:00 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,45 @@
+Enable a webapp to get the optimal `http` implementation based on how
+it is deployed. Also provide a way to get the `baseUrl` of the
+deployment.
+
+Ways one might deploy a node-based webapp:
+
+* [http](https://nodejs.org/api/http.html)
+* [node-fastcgi](https://github.com/fbbdev/node-fastcgi)
+* CGI (in the future just as proof of concept, please don’t use in production)
+* Passenger
+
+# Usage
+
+    const httpAutodetect = require('http-autodetect');
+    
+    const app = function (req, res) {
+        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8', });
+	// It is recommended to use a framework like express to calculate
+	// the host and protocol of your deployment. getBaseUrl() only
+	// solves the path portion of the deployment.
+	res.end(`Hello, World! I am deployed at https://${req.headers.host}${httpAutodetect.getBaseUrl(req)}`);
+    };
+    
+    httpAutodetect(app);
+
+## Express Usage
+
+You need extra middleware to get Express to respect
+`getBaseUrl()`. See
+[express-http-autodetect](https://github.com/binki/express-http-autodetect)
+for that or use this snippet:
+
+    const expressHttpAutodetect = require('express-http-autodetect');
+    
+    // If you followed the Express-4 migration guide or used
+    // express-generator, your app definition will be its own
+    // CommonJS module.
+    const app = require('./app');
+    
+    expressHttpAutodetect(app);
+
+Note the Connect is not supported as it does not have the concept of
+managing `baseUrl`/mounting sub-apps. It only chains handlers and is
+missing the sort of routing handling that proper `baseUrl` handling
+requires.
---
+++ index.js
@@ -0,0 +1,64 @@
+/* -*- mode: js2; indent-tabs-mode: nil; -*- */
+'use strict';
+
+var fs = require('fs');
+var process = require('process');
+var S_IFSOCK;
+
+S_IFSOCK = (fs.constants || {}).S_IFSOCK;
+if (S_IFSOCK === undefined) {
+    S_IFSOCK = (process.binding('constants') || {}).S_IFSOCK;
+}
+if (S_IFSOCK === undefined) {
+    console.log('hi');
+    console.warn('Unable to find S_IFSOCK constant. FastCGI will not be detected.');
+}
+
+module.exports = function (app) {
+    // Autodetect node-fastcgi
+    // https://github.com/fbbdev/node-fastcgi/issues/10#issuecomment-238019124
+    if (fs.fstatSync(0).mode & S_IFSOCK) {
+        require('node-fastcgi').createServer(app).listen();
+        return;
+    }
+
+    // Autodetect CGI (in the future)
+    if (process.env.SCRIPT_NAME) {
+        console.log('Pure CGI mode is not supported yet.');
+    }
+
+    // The way people apparently normally launch node web servers:
+    var http = require('http');
+
+    var port = (typeof app.get === 'function' ? app.get('port') : undefined)
+            || process.env.PORT
+            || 3000;
+    http.createServer(app).listen(port, function () {
+        console.log('Listening on port ' + port);
+    });
+};
+
+var staticBaseUrl;
+
+module.exports.getBaseUrl = function (req) {
+    // node-fastcgi: https://github.com/fbbdev/node-fastcgi/issues/11
+    if (req.socket.params) {
+        return req.socket.params.SCRIPT_NAME;
+    }
+
+    if (staticBaseUrl === undefined) {
+        if (process.env.SCRIPT_NAME) {
+            // pure CGI
+            staticBaseUrl = process.env.SCRIPT_NAME;
+        } else if (process.env.PASSENGER_BASE_URI) {
+            // Passenger,
+            // https://github.com/remko/base-uri/blob/ff560528afd75910a3bdba7a917383fa347ce2b6/index.js#L18
+            // https://github.com/phusion/passenger/blob/69878eb58cc6273397755e62a99bb7394ab66b6d/test/stub/node/app.js#L23
+            staticBaseUrl = process.env.PASSENGER_BASE_URI;
+        } else {
+            // node http module
+            staticBaseUrl = '/';
+        }
+    }
+    return staticBaseUrl;
+};
