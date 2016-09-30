41cc011c90db2ab795e9a2c3edddf4c5eae1e4d1
Nathan Phillip Brink <binki@gentoo.org>
2016-09-30 03:00:20 +0000

    Set options through module property.
    Discussed in #6.
    
    Now the way to set options is to modify the options
    property of the module. This is to support deployment
    scripts scenario/dependency injection.
    
    Also, this change means that platforms do not need
    to check for this.options to discover options.
    Instead, they more clearly/conveniently are passed
    options as the second argument to their serve()
    function.

--- platform/http.js
+++ platform/http.js
@@ -5,11 +5,11 @@ module.exports = {
     detect: function () {
         return true;
     },
-    serve: function (app) {
+    serve: function (app, options) {
         // The way people apparently normally launch node web servers:
         var http = require('http');
 
-        var port = this.options.port
+        var port = options.port
                 || 3000;
         http.createServer(app).listen(port, function () {
             console.log('Listening on port ' + port);
